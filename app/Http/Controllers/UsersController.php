93b831334eaae1834770eeba6b32a60c7be5f937
aaron <853126386@qq.com>
2020-02-12 06:52:43 +0800

    显示上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //个人中心
@@ -25,9 +26,16 @@ class UsersController extends Controller
      * @param User $user
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user,ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $data=$request->all();
+        if($request->avatar){
+            $result=$uploader->save($request->avatar,'avatars',$user->id);
+            if($result){
+                $data['avatar']=$result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
