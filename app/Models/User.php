74198398089a5c73691d05c856dff1fbef7a1cb4
aaron <853126386@qq.com>
2020-02-23 15:06:13 +0800

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use App\Models\Topic;
+use Auth;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     /**
@@ -15,7 +16,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * markEmailAsVerified() 将用户标示为已认证；
      * sendEmailVerificationNotification() 发送 Email 认证的消息通知，触发邮件的发送。
      */
-    use Notifiable,MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -43,6 +48,22 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
+
 
     public function topics(){
         return $this->hasMany(Topic::class);
