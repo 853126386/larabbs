cceb644892c1bc3ac501eb453a579477d06f43c3
aaronlee <853126386@qq.com>
2020-07-25 13:44:49 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,21 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
 
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+
+    $api->group([
+        'middleware'=>'api.throttle',
+        'limit'=>config('api.rate_limits.sign.limit'),
+        'expire'=>config('api.rate_limits.sign.expire')
+    ],function ($api){
+    // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
+
 });
 
 
