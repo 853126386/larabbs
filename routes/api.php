353671a3b0204d7a2757f254a267dc5d0c7bfd55
aaronlee <853126386@qq.com>
2020-07-26 23:32:55 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
 
@@ -40,6 +41,26 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+
+
+
+    $api->group([
+       'middleware'=>'api.throttle',
+        'limit'=>config('api.rate_limits.access.limit'),
+        'expires'=>config('api.rate_limits.access.expires')
+    ],function ($api){
+        //游客可以访问
+
+        //需要token才能访问
+        $api->group([
+            'middleware'=>'api.auth'
+        ],function ($api){
+
+
+            $api->get('user','UsersController@me')->name('api.user.show');
+        });
+
     });
 
 });
