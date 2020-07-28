bd606ba494f5596a62eabf7d928d579c823c1d40
aaron <853126386@qq.com>
2020-07-28 17:12:59 +0800

    topic update api

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,76 +4,55 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = [
-        'title',
-        'body',
-        'category_id',
-        'excerpt',
-        'slug'
-    ];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
-    /**
-     * 本地作用域
-     * @param $query
-     * @param $order
-     * @return mixed
-     */
-    public function scopeWithOrder($query,$order)
-    {
-        if($order=='recent'){
-            $query->recent();
-        }else{
-            $query->recentReplied();
-        }
-        return $query;
-    }
-
-    /**
-     * 更新时间排序本地作用域
-     * @param $query
-     * @return mixed
-     */
-    public function scopeRecentReplied($query){
-        return $query->orderBy('updated_at','desc');
-    }
-
-    /**
-     * 创建时间排序本地作用域
-     * @param $query
-     * @return mixed
-     */
-    public function scopeRecent($query){
-        return $query->orderBy('created_at','desc');
-    }
-
-
-    public function link($params = []){
-        return route('topics.show',array_merge([$this->id,$this->slug],$params));
-    }
-
-
-    /**
-     * 分类关联模型
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function category(){
-        return $this->belongsTo(Category::class,'category_id','id');
-    }
-
-    /**用户关联模型
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user(){
-        return $this->belongsTo(User::class,'user_id','id');
-    }
-
-    /**
-     * 回复关联模型
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query = $this->recent();
+                break;
+
+            default:
+                $query = $this->recentReplied();
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 }
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,7 +9,9 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-         return $user->isAuthorOf($topic);//判断user_id是否是登入用户的user_id
+
+//         return $user->isAuthorOf($topic);//判断user_id是否是登入用户的user_id
+         return false;//判断user_id是否是登入用户的user_id
     }
 
     public function destroy(User $user, Topic $topic)
